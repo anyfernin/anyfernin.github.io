eda6391cc6215e35984b9fcd4903942b89f723fe
Ana Fernanda <A01411484@itesm.mx>
2023-02-17 09:05:49 -0600

    Calculadora completa

--- Operaciones.js
+++ Operaciones.js
@@ -1,33 +1,44 @@
-function calculate() {
+function calculate(operation) {
     var n1 = parseFloat(document.getElementById("n1").value);
     var n2 = parseFloat(document.getElementById("n2").value);
-    var op = document.getElementById("operador").value;
-    var result;
 
-    if (op == "Suma") {
+    if(operation == 0) {
+      var op = document.getElementById("operador").value;
+    }
+    
+    
+    
+    var result;
+    
+
+    if (op == "Suma" || operation == 1) {
       result = n1 + n2;
-    } else if (op == "Resta") {
+    } else if (op == "Resta" || operation == 2) {
       result = n1 - n2;
-    } else if (op == "Multiplicación") {
+    } else if (op == "Multiplicación" || operation == 3) {
       result = n1 * n2;
-    } else if (op == "División") {
+    } else if (op == "División" || operation == 4) {
       result = n1 / n2;
     }
 
     document.getElementById("resultado").innerHTML = result;
-  }
+}
 
-function calculateComplex() {
+function calculateComplex(operation) {
   var n1 = parseFloat(document.getElementById("n1").value);
-  var n2 = parseFloat(document.getElementById("n2").value);
-  var op = document.getElementById("operador").value;
+  
+  if(operation == 0) {
+    var op = document.getElementById("operador").value;
+  }
+  
   var result;
 
-  if (op == "Cuadrado") {
+  if (op == "Cuadrado" || operation == 1) {
     result = n1 * n1;
-  } else if (op == "Raiz") {
+  } else if (op == "Raiz" || operation == 2) {
     result = Math.sqrt(n1);
-  } else if (op == "Potencia") {
+  } else if (op == "Potencia" || operation == 3) {
+    var n2 = parseFloat(document.getElementById("n2").value);
     result = Math.pow(n1, n2);
   } 
 
